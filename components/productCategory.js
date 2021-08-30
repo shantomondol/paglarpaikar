d5dacec6b54ad919e7af3b0c29365d1f47b8122b
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-08-30 20:42:29 +0600

    add more js file

--- components/productCategory.js
+++ components/productCategory.js
@@ -1,25 +1,98 @@
+const productCategorysData = [
+    {        
+        id: 1,
+        photo: "images/chal-dal.png",
+        name: "চাল-ডাল",
+        link: "chal-dal.html"
+    },
+    {        
+        id: 2,
+        photo: "images/oil.png",
+        name: "তেল",
+        link: "oil.html"
+    },
+    {        
+        id: 3,
+        photo: "images/spices.png",
+        name: "মশলা",
+        link: "spices.html"
+    },
+    {        
+        id: 4,
+        photo: "images/salt-sugar.png",
+        name: "লবন এবং চিনি",
+        link: "salt-sugar.html"
+    },
+    {        
+        id: 5,
+        photo: "images/ready-mix.png",
+        name: "রেডি মিক্স",
+        link: "ready-mix.html"
+    },
+    {        
+        id: 6,
+        photo: "images/aata-moyda-suji.png",
+        name: "আটা, ময়দা ও সুজি",
+        link: "aata-moyda-suji.html"
+    },
+    {        
+        id: 7,
+        photo: "images/noodles.png",
+        name: "নুডুলস",
+        link: "noodles.html"
+    },
+    {        
+        id: 8,
+        photo: "images/soups.png",
+        name: "স্যুপ",
+        link: "soups.html"
+    },
+    {        
+        id: 9,
+        photo: "images/icecreams.png",
+        name: "আইসক্রিম",
+        link: "icecreams.html"
+    },
+    {        
+        id: 10,
+        photo: "images/powder-milk-cream.png",
+        name: "গুঁড়া দুধ এবং ক্রিম",
+        link: "powder-milk-cream.html"
+    },
+    {        
+        id: 11,
+        photo: "images/personal-care.png",
+        name: "স্বাস্থ্য এবং সৌন্দর্য",
+        link: "personal-care.html"
+    }
+];
+
+
+
+
+
 function productCategoryTemplate(productCategory) {
     return `
-        <div class="active tab-pane text-center text-style" id="tab2">
-            <div class=" con-w3l">
-                <div class="col-md-3 pro-1">
-                    <div class="col-m">
-                        <a href="${productCategory.link}" class="offer-img">
-                            <img src="${productCategory.photo}" class="img-responsive" alt="${productCategory.name}">
-                        </a>
-                       
-                            <div class="mid-1">                                
-                                    <h6>${productCategory.name}</h6>                              
-                            </div>
-                    </div>
-                </div>               
-                <div id="${productCategory.id}"></div>
-            </div>
+    <div class="active tab-pane text-center text-style" id="tab2">
+        <div class=" con-w3l">
+            <div class="col-md-3 pro-1">
+                <div class="col-m">
+                    <a href="${productCategory.link}" class="offer-img">
+                        <img src="${productCategory.photo}" class="img-responsive" alt="${productCategory.name}">
+                    </a>
+                    
+                        <div class="mid-1">                                
+                                <h6>${productCategory.name}</h6>                              
+                        </div>
+                </div>
+            </div>               
+            <div id="${productCategory.id}"></div>
         </div>
+    </div>
 
     `;
 }
 
 document.getElementById("productCategory").innerHTML = `
-    ${productCategoryData.map(productCategoryTemplate).join("")}  
+    ${productCategorysData.map(productCategoryTemplate).join("")}
   `;
