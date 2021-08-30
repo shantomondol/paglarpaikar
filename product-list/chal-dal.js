d9776e822834944a895030c63f0e83a0b1db3c59
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-08-30 17:36:19 +0600

    change link location

--- product-list/chal-dal.js
+++ product-list/chal-dal.js
@@ -1,7 +1,7 @@
 const productData = [
     {        
         id: 1,
-        photo: "../images/oil.png",
+        photo: "images/oil.png",
         name: "তেল",
         storePrice: "972",
         ourPrice: "972"
