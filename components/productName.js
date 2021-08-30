277e670c9f8f2bac2e6edc8b555212f30891de7c
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-08-30 17:38:02 +0600

    change img link location

--- components/productName.js
+++ components/productName.js
@@ -1,76 +1,76 @@
 const productCategoryData = [
     {        
         id: 1,
-        photo: "../images/chal-dal.png",
+        photo: "images/chal-dal.png",
         name: "চাল-ডাল",
         link: "chal-dal.html"
     },
     {        
         id: 2,
-        photo: "../images/oil.png",
+        photo: "images/oil.png",
         name: "তেল",
         itemPrice1: "972",
         ourPrice: "972"
     },
     {        
         id: 3,
-        photo: "../images/spices.png",
+        photo: "images/spices.png",
         name: "মশলা",
         itemPrice1: "972",
         ourPrice: "972"
     },
     {        
         id: 4,
-        photo: "../images/salt-sugar.png",
+        photo: "images/salt-sugar.png",
         name: "লবন এবং চিনি",
         itemPrice1: "972",
         ourPrice: "972"
     },
     {        
         id: 5,
-        photo: "../images/ready-mix.png",
+        photo: "images/ready-mix.png",
         name: "রেডি মিক্স",
         itemPrice1: "972",
         ourPrice: "972"
     },
     {        
         id: 6,
-        photo: "../images/aata-moyda-suji.png",
+        photo: "images/aata-moyda-suji.png",
         name: "আটা, ময়দা ও সুজি",
         itemPrice1: "972",
         ourPrice: "972"
     },
     {        
         id: 7,
-        photo: "../images/noodles.png",
+        photo: "images/noodles.png",
         name: "নুডুলস",
         itemPrice1: "972",
         ourPrice: "972"
     },
     {        
         id: 8,
-        photo: "../images/soups.png",
+        photo: "images/soups.png",
         name: "স্যুপ",
         itemPrice1: "972",
         ourPrice: "972"
     },
     {        
         id: 9,
-        photo: "../images/icecreams.png",
+        photo: "images/icecreams.png",
         name: "আইসক্রিম",
         itemPrice1: "972",
         ourPrice: "972"
     },
     {        
         id: 10,
-        photo: "../images/powder-milk-cream.png",
+        photo: "images/powder-milk-cream.png",
         name: "গুঁড়া দুধ এবং ক্রিম",
         itemPrice1: "972",
         ourPrice: "972"
     },
     {        
         id: 11,
-        photo: "../images/personal-care.png",
+        photo: "images/personal-care.png",
         name: "স্বাস্থ্য এবং সৌন্দর্য",
         itemPrice1: "972",
         ourPrice: "972"
