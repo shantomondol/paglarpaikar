308cbca4c5bf0a0e26ec6487413dfce516ea82dc
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-09-17 11:58:49 +0600

    data add in saucesData file

---
+++ product-list/sauces.js
@@ -0,0 +1,86 @@
+const productData = [
+    {
+        photo: "images/sauces/pran-chili-sauce-340-ml.jpg",
+        name: "প্রাণ চিলি সস ৩৪০ গ্রাম",
+        storePrice: "105",
+        ourPrice: "90"
+    },
+    {
+        photo: "images/sauces/pran-soya-sauce-280-ml.jpg",
+        name: "প্রাণ সয়া সস ২৮০ মিলি",
+        storePrice: "65",
+        ourPrice: "60"
+    },    
+    {
+        photo: "images/sauces/pran-hot-tomato-sauce-1kg.jpg",
+        name: "প্রান হট টমেটো সস ১কেজি",
+        storePrice: "190",
+        ourPrice: "175"
+    },
+    {
+        photo: "images/sauces/pran-hot-tomato-sauce-3kg.jpg",
+        name: "প্রান হট টমেটো সস ৩কেজি",
+        storePrice: "390",
+        ourPrice: "370"
+    },    
+    {
+        photo: "images/sauces/pran-hot-tomato-sauce-5kg.jpg",
+        name: "প্রান হট টমেটো সস ৫কেজি",
+        storePrice: "600",
+        ourPrice: "570"
+    },    
+    {
+        photo: "images/sauces/fish-sauce-740ml.jpg",
+        name: "ফিশ সস ৭৫০ মিলি",
+        storePrice: "300",
+        ourPrice: "240"
+    },
+    {
+        photo: "images/sauces/oyster-sauce-350ml.jpg",
+        name: "ওস্টার সস ৩৫০ মিলি",
+        storePrice: "200",
+        ourPrice: "160"
+    },    
+    {
+        photo: "images/sauces/barbecue-sauce.jpg",
+        name: "বারবিকিউ সস",
+        storePrice: "300",
+        ourPrice: "275"
+    },
+    {
+        photo: "images/sauces/pran-hot-tomato-sauce-295-gm.jpg",
+        name: "প্রান হট টমেটো সস ২৯৫ গ্রাম",
+        storePrice: "80",
+        ourPrice: "72"
+    },
+    {
+        photo: "images/sauces/pran-hot-tomato-sauce-750gm.jpg",
+        name: "প্রান হট টমেটো সস ৭৫০ গ্রাম",
+        storePrice: "145",
+        ourPrice: "135"
+    },
+    {
+        photo: "images/sauces/pran-hot-tomato-sauce-215-gm.jpg",
+        name: "প্রান হট টমেটো সস ২১৫ গ্রাম",
+        storePrice: "60",
+        ourPrice: "50"
+    },
+    {
+        photo: "images/sauces/pran-hot-tomato-sauce-340-gm.jpg",
+        name: "প্রান হট টমেটো সস ৩৪০ গ্রাম",
+        storePrice: "90",
+        ourPrice: "80"
+    },
+    {
+        photo: "images/sauces/pran-green-chilli-sauce-340gm.jpg",
+        name: "প্রাণ গ্রীন চিলি সস ৩৪০ গ্রাম",
+        storePrice: "80",
+        ourPrice: "72"
+    },
+    {
+        photo: "images/sauces/lobster-sauce-285 ml.jpg",
+        name: "গলদা চিংড়ি মাছের সস ২৮৫ মিলি",
+        storePrice: "200",
+        ourPrice: "160"
+    }
+];
