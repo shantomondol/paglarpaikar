b29e17318f401ae533ed5a842b308daafed6c80f
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-09-17 10:46:59 +0600

    data add in salt-sugarData file

--- product-list/salt-sugar.js
+++ product-list/salt-sugar.js
@@ -1,9 +1,20 @@
 const productData = [
-    {        
-        id: 1,
-        photo: "images/oil.png",
-        name: "তেল",
-        storePrice: "972",
-        ourPrice: "972"
+    {
+        photo: "images/salt-sugar/chini-khola.jpg",
+        name: "চিনি (খোলা) ১ কেজি",
+        storePrice: "75",
+        ourPrice: "72"
+    },
+    {
+        photo: "images/salt-sugar/dolphin-mota-lobon.jpg",
+        name: "ডলফিন মোটা লবণ ১ কেজি",
+        storePrice: "20",
+        ourPrice: "15"
+    },
+    {
+        photo: "images/salt-sugar/chini-khola.jpg",
+        name: "মোল্লা সুপার লবণ ১ কেজি",
+        storePrice: "38",
+        ourPrice: "28"
     }
 ];
