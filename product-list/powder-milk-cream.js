389f4e31d02881b44230d41165577c21b0f47a11
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-09-17 12:48:21 +0600

    data add in powder-milk-creamData file

--- product-list/powder-milk-cream.js
+++ product-list/powder-milk-cream.js
@@ -1,9 +1,74 @@
 const productData = [
-    {        
-        id: 1,
-        photo: "images/oil.png",
-        name: "তেল",
-        storePrice: "972",
-        ourPrice: "972"
-    }
+    {
+        photo: "images/powder-milk-cream/diploma-full-cream-milk-powder-500-gm.jpg",
+        name: "ডিপ্লোমা গুঁড়া দুধ ৫০০ গ্রাম",
+        storePrice: "350",
+        ourPrice: "335"
+    },
+    {
+        photo: "images/powder-milk-cream/diploma-full-cream-milk-powder-1-kg.jpg",
+        name: "ডিপ্লোমা গুঁড়া দুধ ১ কেজি",
+        storePrice: "690",
+        ourPrice: "660"
+    },
+    {
+        photo: "images/powder-milk-cream/marks-full-cream-milk-400gm.jpg",
+        name: "মার্কস গুঁড়া দুধ ৪০০ গ্রাম",
+        storePrice: "280",
+        ourPrice: "240"
+    },
+    {
+        photo: "images/powder-milk-cream/marks-milk-powder-1-kg.jpg",
+        name: "মার্কস গুঁড়া দুধ ১ কেজি",
+        storePrice: "640",
+        ourPrice: "580"
+    },
+    {
+        photo: "images/powder-milk-cream/dano-power-milk-pack-400-gm.jpg",
+        name: "ডানো গুঁড়া দুধ ৪০০ গ্রাম",
+        storePrice: "300",
+        ourPrice: "275"
+    },
+    {
+        photo: "images/powder-milk-cream/nido-powdar-millk-400gm.jpg",
+        name: "নিডো দুধ ৪০০ গ্রাম",
+        storePrice: "270",
+        ourPrice: "258"
+    },
+    {
+        photo: "images/powder-milk-cream/nido-powdar-millk-700gm.jpg",
+        name: "নিডো দুধ ৭০০ গ্রাম",
+        storePrice: "550",
+        ourPrice: "525"
+    },
+    {
+        photo: "images/powder-milk-cream/biomil-1-350gm.jpg",
+        name: "বায়োমিল-১ ৩৫০ গ্রাম",
+        storePrice: "470",
+        ourPrice: "425"
+    },
+    {
+        photo: "images/powder-milk-cream/biomil-1-tin-400gm.jpg",
+        name: "বায়োমিল-১ (টিন) ৪০০ গ্রাম",
+        storePrice: "580",
+        ourPrice: "525"
+    },    
+    {
+        photo: "images/powder-milk-cream/greenland-powder-millk-500gm.jpg",
+        name: "গ্রীন ল্যান্ড দুধ ৫০০ গ্রাম",
+        storePrice: "255",
+        ourPrice: "235"
+    },
+    {
+        photo: "images/powder-milk-cream/starship-condensed-milk.jpg",
+        name: "স্টারশিপ কনডেন্সড মিল্ক",
+        storePrice: "70",
+        ourPrice: "64"
+    },    
+    {
+        photo: "images/powder-milk-cream/danish-condensed-milk.jpg",
+        name: "ড্যানিশ কনডেন্সড মিল্ক",
+        storePrice: "70",
+        ourPrice: "64"
+    },
 ];
