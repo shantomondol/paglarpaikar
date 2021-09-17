515adb2ea478c3d69f72a1ef634d637dbb6de057
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-09-17 12:57:25 +0600

    add pickles html, css, js file

--- components/productCategory.js
+++ components/productCategory.js
@@ -1,60 +1,60 @@
 const productCategorysData = [
-    {        
-        id: 1,
+    {
         photo: "images/chal-dal.png",
         name: "চাল-ডাল",
         link: "chal-dal.html"
     },
-    {        
-        id: 2,
+    {
         photo: "images/oil.png",
         name: "তেল",
         link: "oil.html"
     },
-    {        
-        id: 3,
+    {
         photo: "images/spices.png",
         name: "রেডি মিক্স ও মশলা",
         link: "ready-mix-and-spices.html"
     },
-    {        
-        id: 4,
+    {
         photo: "images/salt-sugar.png",
         name: "লবন এবং চিনি",
         link: "salt-sugar.html"
     },
-    {        
-        id: 5,
+    {
         photo: "images/aata-moyda-suji.png",
         name: "আটা, ময়দা ও সুজি",
         link: "aata-moyda-suji.html"
     },
-    {        
-        id: 6,
+    {
         photo: "images/noodles.png",
         name: "নুডুলস",
         link: "noodles.html"
     },
-    {        
-        id: 7,
+    {
         photo: "images/soups.png",
         name: "স্যুপ",
         link: "soups.html"
     },
-    {        
-        id: 8,
+    {
         photo: "images/icecreams.png",
         name: "আইসক্রিম",
         link: "icecreams.html"
     },
-    {        
-        id: 9,
+    {
         photo: "images/powder-milk-cream.png",
         name: "গুঁড়া দুধ এবং ক্রিম",
         link: "powder-milk-cream.html"
+    },    
+    {
+        photo: "images/sauces.png",
+        name: "সস",
+        link: "sauces.html"
+    }, 
+    {
+        photo: "images/pickles.png",
+        name: "আচার",
+        link: "pickles.html"
     },
-    {        
-        id: 10,
+    {
         photo: "images/personal-care.png",
         name: "স্বাস্থ্য এবং সৌন্দর্য",
         link: "personal-care.html"
@@ -77,8 +77,7 @@ function productCategoryTemplate(productCategory) {
                         </div>
                     </a>
                 </div>
-            </div>               
-            <div id="${productCategory.id}"></div>
+            </div>
         </div>
     </div>
 
---
+++ product-list/pickles.js
@@ -0,0 +1,50 @@
+const productData = [
+    {
+        photo: "images/pickles/",
+        name: "তেল",
+        storePrice: "972",
+        ourPrice: "972"
+    },
+    {
+        photo: "images/pickles/",
+        name: "তেল",
+        storePrice: "972",
+        ourPrice: "972"
+    },
+    {
+        photo: "images/pickles/",
+        name: "তেল",
+        storePrice: "972",
+        ourPrice: "972"
+    },
+    {
+        photo: "images/pickles/",
+        name: "তেল",
+        storePrice: "972",
+        ourPrice: "972"
+    },
+    {
+        photo: "images/pickles/",
+        name: "তেল",
+        storePrice: "972",
+        ourPrice: "972"
+    },
+    {
+        photo: "images/pickles/",
+        name: "তেল",
+        storePrice: "972",
+        ourPrice: "972"
+    },
+    {
+        photo: "images/pickles/",
+        name: "তেল",
+        storePrice: "972",
+        ourPrice: "972"
+    },
+    {
+        photo: "images/pickles/",
+        name: "তেল",
+        storePrice: "972",
+        ourPrice: "972"
+    }
+];
