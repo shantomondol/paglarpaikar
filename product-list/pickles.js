ff88fd4e8760b9935f7aa99c7b9a5fb064f5c55d
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-09-17 13:26:34 +0600

    data entry in picklesData file

--- product-list/pickles.js
+++ product-list/pickles.js
@@ -1,50 +1,56 @@
 const productData = [
     {
-        photo: "images/pickles/",
-        name: "তেল",
-        storePrice: "972",
-        ourPrice: "972"
+        photo: "images/pickles/pran-jolpai-pickle-400gm.jpg",
+        name: "প্রাণ জলপাই আচার ৪০০ গ্রাম",
+        storePrice: "135",
+        ourPrice: "128"
     },
     {
-        photo: "images/pickles/",
-        name: "তেল",
-        storePrice: "972",
-        ourPrice: "972"
+        photo: "images/pickles/pran-jolpai-pickle-1kg.jpg",
+        name: "প্রাণ জলপাই আচার ১ কেজি <br><br>",
+        storePrice: "275",
+        ourPrice: "262"
     },
     {
-        photo: "images/pickles/",
-        name: "তেল",
-        storePrice: "972",
-        ourPrice: "972"
+        photo: "images/pickles/pran-mixed-pickle-400-gm.jpg",
+        name: "প্রান মিক্সড আচার ৪০০ গ্রাম",
+        storePrice: "135",
+        ourPrice: "128"
     },
     {
-        photo: "images/pickles/",
-        name: "তেল",
-        storePrice: "972",
-        ourPrice: "972"
+        photo: "images/pickles/pran-mango-pickle-400-gm.jpg",
+        name: "প্রাণ আমের আচার ৪০০ গ্রাম",
+        storePrice: "135",
+        ourPrice: "125"
     },
     {
-        photo: "images/pickles/",
-        name: "তেল",
-        storePrice: "972",
-        ourPrice: "972"
+        photo: "images/pickles/pran-mango-pickle-1kg.jpg",
+        name: "প্রাণ আমের আচার ১ কেজি <br><br>",
+        storePrice: "275",
+        ourPrice: "262"
     },
     {
-        photo: "images/pickles/",
-        name: "তেল",
-        storePrice: "972",
-        ourPrice: "972"
+        photo: "images/pickles/pran-garlic-pickle-300-gm.jpg",
+        name: "প্রান রসুন আচার ৩০০ গ্রাম <br><br>",
+        storePrice: "185",
+        ourPrice: "178"
     },
     {
-        photo: "images/pickles/",
-        name: "তেল",
-        storePrice: "972",
-        ourPrice: "972"
+        photo: "images/pickles/pran-boroi-sweet-pickle-350gm.jpg",
+        name: "প্রাণ বড়ই মিষ্টি আচার ৩৫০ গ্রাম",
+        storePrice: "100",
+        ourPrice: "92"
     },
     {
-        photo: "images/pickles/",
-        name: "তেল",
-        storePrice: "972",
-        ourPrice: "972"
+        photo: "images/pickles/pran-jugol-chutney-300-gm.jpg",
+        name: "প্রাণ যুগল চাটনি ৩০০ গ্রাম <br><br>",
+        storePrice: "130",
+        ourPrice: "122"
+    },
+    {
+        photo: "images/pickles/pran-naga-chilli-pickle-300-gm.jpg",
+        name: "প্রাম নাগা মরিচ আচার ৩০০ গ্রাম",
+        storePrice: "200",
+        ourPrice: "188"
     }
 ];
